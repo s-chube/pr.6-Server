8d3181dcc1cb2452bf877a31c3f3d74f840e0764
s-chube <schubertr@etown.edu>
2015-04-27 19:03:43 -0400

    added existing files

---
+++ server.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <ctype.h>
+
+int main( int argc, char **argv )
+{
+	if(argc != 3)
+		{
+			printf("Usage: ./server <port> <path to root>");	
+			return 0;
+		}
+
+	int port = atoi(argv[1]);
+	const char* dir = argv[2];
+	
+	if(chdir(dir) == -1)
+		{
+			printf("Could not change to directory: %s", dir);
+			return 0;
+		}
+
+	int s1 = -1;
+	int optval = 1;
+	char ip[15] = "127.0.0.1";
+	s1 = socket(AF_INET, SOCK_STREAM,0);
+	
+	struct sockaddr_in address;
+	memset(&address, 0, sizeof(address));
+	address.sin_family = AF_INET;
+	address.sin_port = htons(port);
+	inet_pton(AF_INET, ip, &(address.sin_addr));
+	
+	setsockopt(s1, SOL_SOCKET, SO_REUSEADDR, &optval , sizeof(optval));
+	listen(s1, 1);
+	struct sockaddr_storage otherAddress;
+	socklen_t otherSize = sizeof(otherAddress);
+	int otSoc;
+	otSoc = accept(s1, (struct sockaddr*) &otherAddress, &otherSize);
+	
+	
+	
+	
+}
